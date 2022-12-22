787836fbdfab2cc3c85242da2a02e30a9ce19399
Cristhian Quispe <cristian.jqf@gmail.com>
2022-12-22 08:49:49 -0500

    tercer commit

--- index.js
+++ index.js
@@ -1,17 +1,21 @@
+/**
+* Listado de todos los productos del menú del restaurante
+*/
 
+/**
+* Lógica para crear pedidos y cobrar los pedidos del usuario
+*/
 const usuario = {
   nombre: "Jaime",
   edad: 30,
   deuda: 0
 }
 
-const pedido = []
+let pedido = []
+let costoPedido = 0
 
-// let pedido = []
-// let costoPedido = 0
 
 // Lista todos los productos del menú en un formato amigable
-
 const mostrarMenu = () => {
   console.log(`CÓDIGO - NOMBRE PRODUCTO - COSTO`) 
   for(let producto of productos) {
@@ -21,51 +25,50 @@ const mostrarMenu = () => {
 const pedirProducto = cod => {
   if (!cod) return "Ingrese un código valido"
 
-const productoEncontrado = productos.find(producto => producto.codigo === cod)
-if (!productoEncontrado) return "El producto no existe"
+  const productoEncontrado = productos.find(producto => producto.codigo === cod)
+  if (!productoEncontrado) return "El producto no existe"
 
   pedido.push(productoEncontrado)
   console.log("El producto ha sido agregado a su pedido. Su pedido es:")
   return pedido
 }
 
-// const verPedido = () => pedido
+const verPedido = () => pedido
 
-// const calcularCosto = () => {
-//   let costo = 0
-//   for (producto of pedido) {
-//     costo += producto.costo
-//   }
-//   costoPedido = costo
-//   return costoPedido
-// }
+const calcularCosto = () => {
+  let costo = 0
+  for (producto of pedido) {
+    costo += producto.costo
+  }
+  costoPedido = costo
+  return costoPedido
+}
 
-// const finalizarPedido = () => {
-//   calcularCosto()
-//   usuario.deuda = costoPedido
+const finalizarPedido = () => {
+  calcularCosto()
+  usuario.deuda = costoPedido
 
-//   pedido = []
-//   costoPedido = 0
+  pedido = []
+  costoPedido = 0
 
-//   return `${usuario.nombre}, debes pagar ${usuario.deuda} dólares.`
-// }
+  return `${usuario.nombre}, debes pagar ${usuario.deuda} dólares.`
+}
 
 
-// // Función que permite pagar todo un pedido y entrega cambio si es necesario.
-// const pagarPedido = montoEntregado => {
-//   if(typeof montoEntregado === "number") {
-//     if (montoEntregado < usuario.deuda) {
-//       return `No te alcanza para pagar tu pedido.`
-//     } else if (montoEntregado === usuario.deuda) {
-//       usuario.deuda = 0
-//       return `Tu pedido ha sido pagado.`
-//     } else {
-//       console.log(`Tu pedido ha sido pagado y tu cambio es de ${montoEntregado - usuario.deuda}.`)
-//       usuario.deuda = 0
-//       return "Deuda pagada"
-//     }
-//   } else {
-//     return "Dato ingresado de forma erronea"
-//   }  
-// } s
-// {
+//  Función que permite pagar todo un pedido y entrega cambio si es necesario.
+const pagarPedido = montoEntregado => {
+  if(typeof montoEntregado === "number") {
+    if (montoEntregado < usuario.deuda) {
+      return `No te alcanza para pagar tu pedido.`
+    } else if (montoEntregado === usuario.deuda) {
+      usuario.deuda = 0
+      return `Tu pedido ha sido pagado.`
+    } else {
+      console.log(`Tu pedido ha sido pagado y tu cambio es de ${montoEntregado - usuario.deuda}.`)
+      usuario.deuda = 0
+      return "Deuda pagada"
+    }
+  } else {
+    return "Dato ingresado de forma erronea"
+  }  
+} 
